fea36af0707ee0bc100998fc17a72b9a6df77202
Aliwoto <woto@kaizoku.cyou>
2022-12-06 18:16:47 +0330

    Refactor the code and convert the question methods to public.
    Signed-off-by: Aliwoto <woto@kaizoku.cyou>

--- Program.cs
+++ Program.cs
@@ -5,68 +5,76 @@
 //Question5();
 //Question6();
 
-using CW1;
+namespace CW1;
 
-QueraQuestion17675.Main(null);
-
-
-static void Question1()
+public class Program
 {
-    var radius = Convert.ToSingle(Console.ReadLine());
-    Console.WriteLine("Circumference: " + 2 * radius * Math.PI);
-    Console.WriteLine("Side: " + radius * radius * Math.PI);
-}
-
-
-static void Question2()
-{
-    var hours = Convert.ToInt32(Console.ReadLine());
-    var mins = Convert.ToInt32(Console.ReadLine());
-    var secs = Convert.ToInt32(Console.ReadLine());
-
-    Console.WriteLine((hours * 60 * 60) + (mins * 60) + secs);
-}
-
-static void Question3()
-{
-    var salary = Convert.ToSingle(Console.ReadLine());
-    salary -= salary * 0.1f;
-    salary -= salary * 0.06f;
-    Console.WriteLine(salary);
-}
-
-static void Question4()
-{
-    string totalStr = "";
-    //foreach (var _ in Enumerable.Range(0, 7))
-    for (int i = 0; i < 7; i++)
+    public static void Main(string[] args)
     {
-        totalStr += Console.ReadKey().KeyChar.ToString();
+        QueraQuestion17675.Main(args);
     }
 
-    var isNum = int.TryParse(totalStr, out var theNum);
-    Console.WriteLine("\n" + isNum);
-    Console.WriteLine(theNum);
+
+    public static void Question1()
+    {
+        var radius = Convert.ToSingle(Console.ReadLine());
+        Console.WriteLine("Circumference: " + 2 * radius * Math.PI);
+        Console.WriteLine("Side: " + radius * radius * Math.PI);
+    }
+
+
+    public static void Question2()
+    {
+        var hours = Convert.ToInt32(Console.ReadLine());
+        var mins = Convert.ToInt32(Console.ReadLine());
+        var secs = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine((hours * 60 * 60) + (mins * 60) + secs);
+    }
+
+    public static void Question3()
+    {
+        var salary = Convert.ToSingle(Console.ReadLine());
+        salary -= salary * 0.1f;
+        salary -= salary * 0.06f;
+        Console.WriteLine(salary);
+    }
+
+    public static void Question4()
+    {
+        string totalStr = "";
+        //foreach (var _ in Enumerable.Range(0, 7))
+        for (int i = 0; i < 7; i++)
+        {
+            totalStr += Console.ReadKey().KeyChar.ToString();
+        }
+
+        var isNum = int.TryParse(totalStr, out var theNum);
+        Console.WriteLine("\n" + isNum);
+        Console.WriteLine(theNum);
+    }
+
+    public static void Question5()
+    {
+        var theNumber = Convert.ToInt32(Console.ReadLine());
+        int firstNum = theNumber / 100;
+        theNumber -= firstNum * 100;
+        int secondNum = theNumber / 10;
+        theNumber -= secondNum * 10;
+        int thirdNum = theNumber;
+
+
+        Console.WriteLine(firstNum + thirdNum == secondNum);
+    }
+
+    public static void Question6()
+    {
+        var theNumber = Convert.ToInt32(Console.ReadLine());
+        int firstNum = theNumber / 10;
+        int secondNum = theNumber - (firstNum * 10);
+
+        Console.WriteLine(theNumber % firstNum == 0 && theNumber % secondNum == 0);
+    }
+
 }
 
-static void Question5()
-{
-    var theNumber = Convert.ToInt32(Console.ReadLine());
-    int firstNum = theNumber / 100;
-    theNumber -= firstNum * 100;
-    int secondNum = theNumber / 10;
-    theNumber -= secondNum * 10;
-    int thirdNum = theNumber;
-
-
-    Console.WriteLine(firstNum + thirdNum == secondNum);
-}
-
-static void Question6()
-{
-    var theNumber = Convert.ToInt32(Console.ReadLine());
-    int firstNum = theNumber / 10;
-    int secondNum = theNumber - (firstNum * 10);
-
-    Console.WriteLine(theNumber % firstNum == 0 && theNumber % secondNum == 0);
-}
